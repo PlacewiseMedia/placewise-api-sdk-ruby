8f945b0b1eb33e406ab65f6e7882d3d36fc67ec2
Matt Morris <double.emms@gmail.com>
2016-05-17 16:06:07 -0600

    stub out basic methods for client

--- lib/placewise/api/client.rb
+++ lib/placewise/api/client.rb
@@ -1,7 +1,32 @@
 module Placewise
   module Api
     class Client
-      #
+      def initialize(email, password, base_url)
+        @email, @password = email, password
+        @base_url = base_url
+      end
+
+      def login
+        # TODO!!
+        @auth_id, @auth_token = 'foo', 'bar'
+      end
+
+      def get(resource, id = nil)
+        login unless @auth_id
+        url_for(resource)
+      end
+
+      private
+
+      def url_for(path, id = nil)
+        sign "#{@base_url}/#{path}#{"/#{id}" if id}"
+      end
+
+      def sign(url)
+        timestamp = Time.now.to_i
+        token = Digest::MD5.hexdigest("#{timestamp}:#{@auth_token}")
+        "#{url}?auth_id=#{@auth_id}&auth_token=#{token}&timestamp=#{timestamp}"
+      end
     end
   end
 end
